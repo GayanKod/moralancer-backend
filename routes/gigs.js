e4ef8b63895793152e5b4f287902c61a134e8b8e
GayanKod <gayan.lakshan9@gmail.com>
2021-03-29 20:33:55 +0530

    Create Gig Create Router

---
+++ routes/gigs.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+let Gig = require("../models/gig");
+
+
+//http:localhost:3001/gigs/create
+router.route("create").post((req,res) => {
+
+    const gigTitle = req.body.gigTitle;
+    const gigCategory = req.body.gigCategory;
+    const gigSearchTags = req.body.gigSearchTags;
+    const gigBasicPriceDesc = req.body.gigBasicPriceDesc;
+    const gigBasicPrice = Number(req.body.gigBasicPrice);
+    const gigStandardPriceDesc = req.body.gigStandardPriceDesc;
+    const gigStandardPrice = Number(req.body.gigStandardPrice);
+    const gigPremiumPriceDesc = req.body.gigPremiumPriceDesc;
+    const gigPremiumPrice = Number(req.body.gigPremiumPrice);
+    const gigDesc = req.body.gigDesc;
+    const gigReq = req.body.gigReq;
+
+    const newGig = new Gig({
+        gigTitle,
+        gigCategory,
+        gigSearchTags,
+        gigBasicPriceDesc,
+        gigBasicPrice,
+        gigBasicPrice,
+        gigStandardPriceDesc,
+        gigStandardPrice,
+        gigPremiumPriceDesc,
+        gigPremiumPrice,
+        gigDesc,
+        gigReq
+    })
+
+    newGig.save().then(()=>{
+        res.json("Gig Created");
+    }).catch((err) => {
+        console.log(err);
+    })
+
+})
+
+
+
+
+
+module.exports = router;
